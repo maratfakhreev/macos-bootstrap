2906c7c763f009412938ce09c1f90081d2ab6438
Marat Fakhreev <marat.fakhreev@gmail.com>
2021-08-22 01:36:53 +0300

    Upgrade ruby version to 3.0.1

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -6,7 +6,7 @@ source "$macos_bootstrap/modules/functions.bash"
 info_echo "Enable rbenv alias"
 eval "$(rbenv init -)"
 
-ruby_version="2.7.2"
+ruby_version="3.0.1"
 
 if test -z "$(rbenv versions --bare|grep $ruby_version)"; then
   info_echo "Install Ruby $ruby_version"
