3faa58a942bf8a5553ade349b724cb1b29461141
Marat Fakhreev <marat.fakhreev@gmail.com>
2023-05-03 12:40:30 +0200

    Add Homebrew to PATH into brew.bash

--- modules/brew.bash
+++ modules/brew.bash
@@ -6,6 +6,10 @@ source "$macos_bootstrap/modules/functions.bash"
 info_echo "Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+info_echo "Add Homebrew to PATH"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/maratfakhreev/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 info_echo "Install Brew formalue"
 brew tap "Homebrew/bundle" 2> /dev/null
 brew bundle --file="$macos_bootstrap/Brewfile"
