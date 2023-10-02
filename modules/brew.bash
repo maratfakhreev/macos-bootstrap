57abc8a971c8e7f0a0bc4c6211ada21b16137c53
Marat Fakhreev <marat.fakhreev@gmail.com>
2023-10-02 16:11:34 +0200

    Update dotfiles. Make scripts fixes

--- modules/brew.bash
+++ modules/brew.bash
@@ -7,7 +7,7 @@ info_echo "Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 info_echo "Add Homebrew to PATH"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/maratfakhreev/.zprofile
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 info_echo "Install Brew formalue"
