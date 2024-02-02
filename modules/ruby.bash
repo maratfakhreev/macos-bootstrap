9817c5a908fbc239119da0db9933fdb42be0138a
Marat Fakhreev <marat.fakhreev@gmail.com>
2024-02-02 12:46:22 +0100

    Update ruby to version 3.2.3

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -6,7 +6,7 @@ source "$macos_bootstrap/modules/functions.bash"
 info_echo "Enable rbenv alias"
 eval "$(rbenv init -)"
 
-ruby_version="3.2.1"
+ruby_version="3.2.3"
 
 if test -z "$(rbenv versions --bare|grep $ruby_version)"; then
   info_echo "Install Ruby $ruby_version"
